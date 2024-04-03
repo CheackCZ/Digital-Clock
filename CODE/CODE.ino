89f75795daaa0d4037582705c33539f3075ca606
Faltun <ondra.faltin@gmail.com>
2024-04-03 17:29:59 +0200

    Přidání metod pro přeměnu string inputu na čísla na displej + kontrolu formátu inputu a výpis inputu. Pozměněna struktura v loop a setup metodě. Zkouška práce s regexem pomocí knihovny regexp, ovšem to nefunguje => tudíž používám metodu jinak.

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -31,49 +31,185 @@
   // Time input
   String time = "10:52";
 
-  // Default Methods
-  void setup() {
-      // Pin segments initialization.
-    for (int i = 0; i < 8; i++) {
-        pinMode(segmentPins[i], OUTPUT);
-    }
-    // Pin digits initialization
-    for (int i = 0; i < 4; i++) {
-        pinMode(digitPins[i], OUTPUT);
-        digitalWrite(digitPins[i], digitOFF);
-    }
 
-    // Initialize serial communication
-    Serial.begin(9600);
-    // Print the string to the serial monitor for control.
-    Serial.println("Time input: " + time);
-    // Trying to convert the string time to individual
-    ConvertString(time);
+// Default Methods
+void setup() {
+    // Pin segments initialization.
+  for (int i = 0; i < 8; i++) {
+      pinMode(segmentPins[i], OUTPUT);
+  }
+  // Pin digits initialization
+  for (int i = 0; i < 4; i++) {
+      pinMode(digitPins[i], OUTPUT);
+      digitalWrite(digitPins[i], digitOFF);
   }
 
-  void loop() {
-    Error();
+  // Initialize serial communication
+  Serial.begin(9600);
 
-  /*One(D1);
-
-  Two(D2);
-
-  Three(D3);
-
-  Four(D4);
-
-  Five(D1);
-
-  Six(D2);
-
-  Seven(D3);
-
-  Eight(D4);
-
-  Nine(D1);
-
-  loop();*/
+  PrintInput(time);
 }
+
+void loop() {
+  // Trying to convert the string time to individual ints
+  ConvertString(time);
+}
+
+
+// Method converting string to the numbers to view the time on the led display
+void ConvertString(String input) {
+  if (CheckInput(input)) {
+    Convertion(input);
+  } else {
+      Error();
+  }
+}
+
+// Method with the Actual Conversion
+void Convertion(String input) {
+  // Array with individual characters from string input declaration
+  char numArray[6]; 
+    
+  // Convert the String to a character array
+  input.toCharArray(numArray, 6);
+  
+  // Display each digit on the LED display
+  for (int i = 0; i < input.length(); i++) {
+    if (isdigit(numArray[i])) {
+      if (i > 2) {
+        DisplayDigit(numArray[i], i-1);
+      } else {
+        DisplayDigit(numArray[i], i);
+      }
+    } 
+    delay(1); 
+  }
+
+  // Turn off all digits after displaying
+  AllDigitsDown();
+}
+
+// Method printing the string input
+void PrintInput(String input) {
+   // Array with individual characters from string input declaration
+  char numArray[6]; 
+    
+  // Convert the String to a character array
+  input.toCharArray(numArray, 6);
+
+  for (int i = 0; i < input.length(); i++) {
+    Serial.print(numArray[i]);
+  }
+}
+
+// Method checking the format of given input
+bool CheckInput(const String& input) {
+    if (input.length() != 5) // Input must be in format "HH:MM"
+        return false;
+
+    for (int i = 0; i < 5; i++) {
+        if (i == 2 && input[i] != ':') // Check for colon at position 2
+            return false;
+        else if (i != 2 && !isdigit(input[i])) // Check for digits elsewhere
+            return false;
+    }
+
+    // Check hours and minutes ranges
+    int hours = (input.substring(0, 2)).toInt();
+    int minutes = (input.substring(3)).toInt();
+    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59)
+        return false;
+
+    return true;
+}
+
+// Method displaying individual digits on individual displays
+void DisplayDigit(uint8_t digit, uint8_t position) {
+  // Turn off all digits
+  AllDigitsDown();
+  // Turn off all segments
+  AllSegmentsDown();
+
+  // Activate the desired digit
+  digitalWrite(digitPins[position], digitON);
+
+  // Activate segments based on the digit to display
+  switch (digit) {
+    case '0':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(E, segmentON);
+      digitalWrite(F, segmentON);
+      break;
+    case '1':
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      break;
+    case '2':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(G, segmentON);
+      digitalWrite(E, segmentON);
+      digitalWrite(D, segmentON);
+      break;
+    case '3':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    case '4':
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(F, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    case '5':
+      digitalWrite(A, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(F, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    case '6':
+      digitalWrite(A, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(E, segmentON);
+      digitalWrite(F, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    case '7':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      break;
+    case '8':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(E, segmentON);
+      digitalWrite(F, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    case '9':
+      digitalWrite(A, segmentON);
+      digitalWrite(B, segmentON);
+      digitalWrite(C, segmentON);
+      digitalWrite(D, segmentON);
+      digitalWrite(F, segmentON);
+      digitalWrite(G, segmentON);
+      break;
+    default:
+      // Display nothing or an error indicator
+      break;
+  }
+}
+
 
 // Method turning on all digit fields
 void AllDigitsDown() {
@@ -104,20 +240,6 @@ void AllSegmentsUp() {
 }
 
 
-// Method converting string to the numbers to view the time on the led display
-void ConvertString(String input) {
-  char numArray[6]; 
-  
-  // Convert the String to a character array
-  input.toCharArray(numArray, 6);
-  
-  for (int i = 0; i < input.length(); i++) {
-    Serial.print(numArray[i]);
-    Serial.print(" ");
-  }
-}
-
-
 // Number One showed on given pin with 1 second display delay
 void One(uint8_t pin) {
   digitalWrite(pin, digitON);
@@ -131,7 +253,7 @@ void One(uint8_t pin) {
   digitalWrite(G, segmentOFF);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -150,7 +272,7 @@ void Two(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -169,7 +291,7 @@ void Three(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -188,7 +310,7 @@ void Four(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -207,7 +329,7 @@ void Five(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -226,7 +348,7 @@ void Six(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -245,7 +367,7 @@ void Seven(uint8_t pin) {
   digitalWrite(G, segmentOFF);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -264,7 +386,7 @@ void Eight(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -283,7 +405,7 @@ void Nine(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -302,7 +424,7 @@ void Dot(uint8_t pin) {
   digitalWrite(G, segmentOFF);
   digitalWrite(DP, segmentON);
   
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -321,7 +443,7 @@ void E_char(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
 
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
@@ -340,7 +462,7 @@ void R_char(uint8_t pin) {
   digitalWrite(G, segmentON);
   digitalWrite(DP, segmentOFF);
   
-  delay(1000);
+  delay(1);
 
   AllSegmentsDown();
   AllDigitsDown();
