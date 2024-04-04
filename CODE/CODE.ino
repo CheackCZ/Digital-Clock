6fad1070cf99db8649db6f9d63391a3ff1733fb5
Faltun <ondra.faltin@gmail.com>
2024-04-04 17:01:39 +0200

    I removed the Regexp library, because I'm not using it. I added methods (CurrentTimeCounter()) counting the seconds and work with the time. I also added method to format the time correctly (Format Time). It is working as it should, but the only problem is that it shows only the last number. Next I added somw variables to work with the time properly and made some detail changes in some of the older methods.

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -29,12 +29,18 @@
   uint8_t segmentOFF = HIGH;
 
   // Time input
-  String time = "10:52";
+  String time = "23:58";
+  // Declaration of time variables
+  int hours = 0;
+  int minutes = 0;
+
+  unsigned long previousMillis = 0;  // Variable to store the last time the LED was updated
+  const long interval = 60000;       // Interval (in milliseconds) at which to update the LED (1 minute)
 
 
 // Default Methods
 void setup() {
-    // Pin segments initialization.
+  // Pin segments initialization.
   for (int i = 0; i < 8; i++) {
       pinMode(segmentPins[i], OUTPUT);
   }
@@ -47,19 +53,35 @@ void setup() {
   // Initialize serial communication
   Serial.begin(9600);
 
+  // Declare hours and minutes
+  hours = (time.substring(0, 2)).toInt();
+  minutes = (time.substring(3)).toInt();
+
   PrintInput(time);
 }
 
 void loop() {
-  // Trying to convert the string time to individual ints
-  ConvertString(time);
-}
+  unsigned long currentMillis = millis();  // Get the current time
 
+  // Check if it's time to update the LED
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time the LED was updated
+    previousMillis = currentMillis;
+
+    // Trying to convert the string time to individual ints
+    ConvertString(time);
+
+    Serial.println(FormatTime(hours, minutes));
+  }
+}
 
 // Method converting string to the numbers to view the time on the led display
 void ConvertString(String input) {
   if (CheckInput(input)) {
-    Convertion(input);
+    CurrentTimeCounter();
+    String currentTime = FormatTime(hours, minutes);
+    
+    Convertion(currentTime);
   } else {
       Error();
   }
@@ -84,9 +106,6 @@ void Convertion(String input) {
     } 
     delay(1); 
   }
-
-  // Turn off all digits after displaying
-  AllDigitsDown();
 }
 
 // Method printing the string input
@@ -104,23 +123,23 @@ void PrintInput(String input) {
 
 // Method checking the format of given input
 bool CheckInput(const String& input) {
-    if (input.length() != 5) // Input must be in format "HH:MM"
-        return false;
+  if (input.length() != 5) // Input must be in format "HH:MM"
+      return false;
 
-    for (int i = 0; i < 5; i++) {
-        if (i == 2 && input[i] != ':') // Check for colon at position 2
-            return false;
-        else if (i != 2 && !isdigit(input[i])) // Check for digits elsewhere
-            return false;
-    }
+  for (int i = 0; i < 5; i++) {
+      if (i == 2 && input[i] != ':') // Check for colon at position 2
+          return false;
+      else if (i != 2 && !isdigit(input[i])) // Check for digits elsewhere
+          return false;
+  }
 
-    // Check hours and minutes ranges
-    int hours = (input.substring(0, 2)).toInt();
-    int minutes = (input.substring(3)).toInt();
-    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59)
-        return false;
+  // Check hours and minutes ranges
+  int hours2 = (input.substring(0, 2)).toInt();
+  int minutes2 = (input.substring(3)).toInt();
+  if (hours2 < 0 || hours2 > 23 || minutes2 < 0 || minutes2 > 59)
+      return false;
 
-    return true;
+  return true;
 }
 
 // Method displaying individual digits on individual displays
@@ -136,78 +155,68 @@ void DisplayDigit(uint8_t digit, uint8_t position) {
   // Activate segments based on the digit to display
   switch (digit) {
     case '0':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(E, segmentON);
-      digitalWrite(F, segmentON);
+      Zero();
       break;
     case '1':
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
+      One();
       break;
     case '2':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(G, segmentON);
-      digitalWrite(E, segmentON);
-      digitalWrite(D, segmentON);
+      Two();
       break;
     case '3':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(G, segmentON);
+      Three();
       break;
     case '4':
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(F, segmentON);
-      digitalWrite(G, segmentON);
+      Four();
       break;
     case '5':
-      digitalWrite(A, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(F, segmentON);
-      digitalWrite(G, segmentON);
+      Five();
       break;
     case '6':
-      digitalWrite(A, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(E, segmentON);
-      digitalWrite(F, segmentON);
-      digitalWrite(G, segmentON);
+      Six();
       break;
     case '7':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
+      Seven();
       break;
     case '8':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(E, segmentON);
-      digitalWrite(F, segmentON);
-      digitalWrite(G, segmentON);
+      Eight();
       break;
     case '9':
-      digitalWrite(A, segmentON);
-      digitalWrite(B, segmentON);
-      digitalWrite(C, segmentON);
-      digitalWrite(D, segmentON);
-      digitalWrite(F, segmentON);
-      digitalWrite(G, segmentON);
+      Nine();
       break;
     default:
       // Display nothing or an error indicator
       break;
   }
+}
+
+// Method working with the int variables to work with the time properly
+void CurrentTimeCounter() {
+  minutes++;
+  if (minutes == 60) {
+    minutes = 0;
+    hours++;
+    if (hours == 24) {
+      hours = 0;
+    }
+  }
+}
+
+// Method to format time as string (HH:MM)
+String FormatTime(int hour, int minute) {
+  String formattedTime = "";
+
+  if (hour < 10) {
+    formattedTime += "0"; 
+  }
+  formattedTime += String(hour) + ":";
+
+  if (minute < 10) {
+    formattedTime += "0";
+  }
+  formattedTime += String(minute);
+
+  return formattedTime;
 }
 
 
@@ -239,8 +248,27 @@ void AllSegmentsUp() {
   delay(1000);
 }
 
+// Number Zero 
+void Zero() {
+  //digitalWrite(pin, digitON);
 
-// Number One showed on given pin with 1 second display delay
+  digitalWrite(A, segmentON);
+  digitalWrite(B, segmentON);
+  digitalWrite(C, segmentON);  
+  digitalWrite(D, segmentON);
+  digitalWrite(E, segmentON);
+  digitalWrite(F, segmentON);
+  digitalWrite(G, segmentOFF);
+  digitalWrite(DP, segmentOFF);
+
+  /*delay(1);
+
+  AllSegmentsDown();
+  AllDigitsDown();*/
+}
+
+
+// Number One 
 void One() {
   //digitalWrite(pin, digitON);
 
@@ -259,7 +287,7 @@ void One() {
   AllDigitsDown();*/
 }
 
-// Number Two showed on given pin with 1 second display delay
+// Number Two 
 void Two() {
   //digitalWrite(pin, digitON);
 
@@ -278,7 +306,7 @@ void Two() {
   AllDigitsDown();*/
 }
 
-// Number Three showed on given pin with 1 second display delay
+// Number Three 
 void Three() {
   //digitalWrite(pin, digitON);
 
@@ -297,7 +325,7 @@ void Three() {
   AllDigitsDown();*/
 }
 
-// Number Four showed on given pin with 1 second display delay
+// Number Four 
 void Four() {
   //digitalWrite(pin, digitON);
 
@@ -316,7 +344,7 @@ void Four() {
   AllDigitsDown();*/
 }
 
-// Number Five showed on given pin with 1 second display delay
+// Number Five 
 void Five() {
   //digitalWrite(pin, digitON);
 
@@ -335,7 +363,7 @@ void Five() {
   AllDigitsDown();*/
 }
 
-// Number Six showed on given pin with 1 second display delay
+// Number Six 
 void Six() {
   //digitalWrite(pin, digitON);
 
@@ -354,7 +382,7 @@ void Six() {
   AllDigitsDown();*/
 }
 
-// Number Seven showed on given pin with 1 second display delay
+// Number Seven 
 void Seven() {
   //digitalWrite(pin, digitON);
 
@@ -373,7 +401,7 @@ void Seven() {
   AllDigitsDown();*/
 }
 
-// Number Eight showed on given pin with 1 second display delay
+// Number Eight 
 void Eight() {
   //digitalWrite(pin, digitON);
 
@@ -392,7 +420,7 @@ void Eight() {
   AllDigitsDown();*/
 }
 
-// Number Nine showed on given pin with 1 second display delay
+// Number Nine 
 void Nine() {
   //digitalWrite(pin, digitON);
 
@@ -411,9 +439,9 @@ void Nine() {
   AllDigitsDown();*/
 }
 
-// Number Nine showed on given pin with 1 second display delay
-void Dot() {
-  //digitalWrite(pin, digitON);
+// Dot 
+void Dot(uint8_t pin) {
+  digitalWrite(pin, digitON);
 
   digitalWrite(A, segmentOFF);
   digitalWrite(B, segmentOFF);
@@ -424,10 +452,10 @@ void Dot() {
   digitalWrite(G, segmentOFF);
   digitalWrite(DP, segmentON);
   
-  /*delay(1);
+  delay(1);
 
   AllSegmentsDown();
-  AllDigitsDown();*/
+  AllDigitsDown();
 }
 
 // char E showed on given pin
